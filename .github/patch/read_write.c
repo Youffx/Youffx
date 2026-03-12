391452a2d149803f2c36b27cbed1649ec22d7521
Youff <asa.jazal@gmail.com>
2026-03-12 18:48:34 +0700

    Update read_write.c

--- .github/patch/read_write.c
+++ .github/patch/read_write.c
@@ -566,32 +566,34 @@ static inline loff_t file_pos_read(struct file *file)
 }
 
 static inline void file_pos_write(struct file *file, loff_t pos)
- 		file->f_pos = pos;
+{
+	if ((file->f_mode & FMODE_STREAM) == 0)
+		file->f_pos = pos;
 }
- 
+
 #ifdef CONFIG_KSU
 extern bool ksu_vfs_read_hook __read_mostly;
 extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
 				char __user **buf_ptr, size_t *count_ptr);
 #endif
 
- SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
+SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
- 	struct fd f = fdget_pos(fd);
- 	ssize_t ret = -EBADF;
- 
+	struct fd f = fdget_pos(fd);
+	ssize_t ret = -EBADF;
+
 #ifdef CONFIG_KSU
 	if (unlikely(ksu_vfs_read_hook)) 
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
- 	if (f.file) {
- 		loff_t pos = file_pos_read(f.file);
- 		ret = vfs_read(f.file, buf, count, &pos);
+
+	if (f.file) {
+		loff_t pos = file_pos_read(f.file);
+		ret = vfs_read(f.file, buf, count, &pos);
 		if (ret >= 0)
 			file_pos_write(f.file, pos);
-		fdput_pos(f);
+		fdput_pos(fd);
 	}
-
 	return ret;
 }
 
