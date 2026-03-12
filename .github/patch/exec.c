d4a84fea345dc877b760bb9e58a7096f11e34383
Youff <asa.jazal@gmail.com>
2026-03-12 11:32:31 +0700

    feat: Update C source
    Modify 1 file(s):
      - .github/patch/exec.c

--- .github/patch/exec.c
+++ .github/patch/exec.c
@@ -1897,14 +1897,16 @@ out_ret:
 }
 
 static int do_execveat_common(int fd, struct filename *filename,
-			      struct user_arg_ptr argv,
-			      struct user_arg_ptr envp,
-			      int flags)
-{
-	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
-}
+ 	return retval;
+ }
+ 
+#ifdef CONFIG_KSU
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+				void *argv, void *envp, int *flags);
+#endif
 
-int do_execve_file(struct file *file, void *__argv, void *__envp)
+ int do_execve_file(struct file *file, void *__argv, void *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
