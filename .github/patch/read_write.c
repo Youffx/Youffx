f05f8c79846f899b5189d2f2b7ad98cf53754bc9
Youff <asa.jazal@gmail.com>
2026-03-12 22:24:13 +0700

    feat: Update read_write C source
    Modify 1 file(s):
      - .github/patch/read_write.c

--- .github/patch/read_write.c
+++ .github/patch/read_write.c
@@ -571,8 +571,21 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 		file->f_pos = pos;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_vfs_read_hook __read_mostly;
+extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
+				char __user **buf_ptr, size_t *count_ptr);
+extern bool ksu_vfs_write_hook __read_mostly;
+extern __attribute__((cold)) int ksu_handle_sys_write(unsigned int fd,
+				const char __user **buf_ptr, size_t *count_ptr);
+#endif
+
 ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
 {
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_vfs_read_hook))
+		ksu_handle_sys_read(fd, &buf, &count);
+#endif
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
 
@@ -593,6 +606,10 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 
 ssize_t ksys_write(unsigned int fd, const char __user *buf, size_t count)
 {
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_vfs_write_hook))
+		ksu_handle_sys_write(fd, &buf, &count);
+#endif
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
 
